a0fe677af2b10cf36a0d3d22845c4e5f19f7fdd4
Elva <elvamuyi@gmail.com>
2013-11-20 23:42:44 -0500

    Update view.py: change q2() in correspondence with backend q2_hbase()

--- cloud/view.py
+++ cloud/view.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from cloud.backend import q2_hbase
 import datetime
 
 TEAMID = 'Alvin'
@@ -9,14 +10,18 @@ def hello(request):
 
 def q1(request):
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    html = "<html><body><p>%s, %s</p><p>%s</p></body></html>" % (TEAMID, AWS_ACCOUNT_ID, now)
-    return HttpResponse(html)
+#    html = "<html><body><p>%s, %s</p><p>%s</p></body></html>" % (TEAMID, AWS_ACCOUNT_ID, now)
+#    return HttpResponse(html)
+    response = TEAMID + ", " + AWS_ACCOUNT_ID + "\n" + now
+    return HttpResponse(response)
 
 def q2(request):
     time = request.GET.get('time')
-    return HttpResponse('Time: ' + time)
+    response = q2_hbase(time)
+    return HttpResponse(response)
 
 def q3(request):
     userid_min = request.GET.get('userid_min')
     userid_max = request.GET.get('userid_max')
     return HttpResponse('userid_min: ' + userid_min +' userid_max: ' + userid_max)
+
