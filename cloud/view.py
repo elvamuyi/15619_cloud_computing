ca079205264a2d0bc2d7290990203faaa872eebd
Elva <elvamuyi@gmail.com>
2013-11-22 00:43:50 -0500

    Finalize view.py

--- cloud/view.py
+++ cloud/view.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from cloud.backend import q2_hbase, q3_hbase, q4_hbase
+from cloud.backend import *
 import datetime
 
 TEAMID = 'Alvin'
@@ -10,6 +10,8 @@ def hello(request):
 
 def q1(request):
     now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#    html = "<html><body><p>%s, %s</p><p>%s</p></body></html>" % (TEAMID, AWS_ACCOUNT_ID, now)
+#    return HttpResponse(html)
     response = TEAMID + ", " + AWS_ACCOUNT_ID + "\n" + now
     return HttpResponse(response)
 
@@ -18,7 +20,6 @@ def q2(request):
     response = TEAMID + ", " + AWS_ACCOUNT_ID + "\n"
     response += q2_hbase(time)
     return HttpResponse(response)
-#    return HttpResponse('time: ' + time)
 
 def q3(request):
     userid_min = request.GET.get('userid_min')
@@ -27,10 +28,11 @@ def q3(request):
     response += q3_hbase(userid_min, userid_max)
     return HttpResponse(response)
 #    return HttpResponse('userid_min: ' + userid_min +' userid_max: ' + userid_max)
-    
+
 def q4(request):
     userid = request.GET.get('userid')
     response = TEAMID + ", " + AWS_ACCOUNT_ID + "\n"
     response += q4_hbase(userid)
     return HttpResponse(response)
 #    return HttpResponse('userid: ' + userid)
+
