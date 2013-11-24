32311edb9880c8e8c364adff5715712803c50d7f
Elva <elvamuyi@gmail.com>
2013-11-24 17:06:40 -0500

    Update PersistJsonToTweetTable.java

--- cloud/PersistJsonToTweetTable.java
+++ cloud/PersistJsonToTweetTable.java
@@ -22,7 +22,7 @@ public class PersistJsonToTweetTable {
   public static final String NAME = "persisit_json_to_tweet_table_in_hbase";
   public static final String TABLE = "tweet";
   public static final String[] FAMILY = {"time", "text", "user", "re"};
-  public static final String[] QUALIFIER = {"time", "text", "uid", "is_re", "o_tid"};
+  public static final String[] QUALIFIER = {"time", "text", "uid", "is_re", "o_uid"};
 
   public static class PersistMapper extends Mapper<LongWritable, Text, ImmutableBytesWritable, Writable> {
     
@@ -32,17 +32,19 @@ public class PersistJsonToTweetTable {
       try {
       
         /* Extract related fields from JSON object */
-        JSONObject tweet = (JSONObject) parser.parse(line.toString());  // JSON object Tweets
-        String tid = (String) tweet.get("id_str");                      // tweet id
-        String time = (String) tweet.get("created_at");                 // time of creation
-        String text = (String) tweet.get("text");                       // text
-        JSONObject user = (JSONObject) tweet.get("user");               // JSON object Users
-        String uid = (String) user.get("id_str");                       // user id
-        Boolean is_re = (Boolean) tweet.get("retweeted");               // whether the tweet is retweet
-        String o_tid = "none";                                            // if is retweet, get tweet id of original tweet
-        if (is_re) {
-          JSONObject o_tweet = (JSONObject) tweet.get("retweeted_status");
-          o_tid = (String) o_tweet.get("id_str");
+        JSONObject tweet = (JSONObject) parser.parse(line.toString());      // JSON object Tweets
+        String tid = (String) tweet.get("id_str");                          // tweet id
+        String time = (String) tweet.get("created_at");                     // time of creation
+        String text = (String) tweet.get("text");                           // text
+        JSONObject user = (JSONObject) tweet.get("user");                   // JSON object Users
+        String uid = (String) user.get("id_str");                           // user id
+        JSONObject o_tweet = (JSONObject) tweet.get("retweeted_status");    // if is retweet, get user id of original tweet
+        String o_uid = "none";
+        Boolean is_re = false;
+        if (o_tweet != null) {
+          is_re = true;
+          JSONObject o_user = (JSONObject) o_tweet.get("user");
+          o_uid = (String) o_user.get("id_str");
         }
 
         Put put = new Put(Bytes.toBytes(tid));  // use tweet id as row key
@@ -50,7 +52,7 @@ public class PersistJsonToTweetTable {
         put.add(Bytes.toBytes(FAMILY[1]), Bytes.toBytes(QUALIFIER[1]), Bytes.toBytes(text));  // Column Family: text, Column: text, Value: text
         put.add(Bytes.toBytes(FAMILY[2]), Bytes.toBytes(QUALIFIER[2]), Bytes.toBytes(uid));   // Column Family: user, Column: uid, Value: uid
         put.add(Bytes.toBytes(FAMILY[3]), Bytes.toBytes(QUALIFIER[3]), new byte[]{(byte) (is_re?1:0)}); // Column Family: re, Column: is_re, Value: is_re
-        put.add(Bytes.toBytes(FAMILY[3]), Bytes.toBytes(QUALIFIER[4]), Bytes.toBytes(o_tid)); // Column Family: re, Column: o_tid, Value: o_tid
+        put.add(Bytes.toBytes(FAMILY[3]), Bytes.toBytes(QUALIFIER[4]), Bytes.toBytes(o_uid)); // Column Family: re, Column: o_uid, Value: o_uid
         context.write(new ImmutableBytesWritable(Bytes.toBytes(tid)), put);
         
       } catch (Exception e) {
