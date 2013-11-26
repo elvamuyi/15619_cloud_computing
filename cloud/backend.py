3d56988355b4878e922d36087f1c6d0c1b55c0db
Elva <elvamuyi@gmail.com>
2013-11-26 01:17:48 -0500

    Optimize q3

--- cloud/backend.py
+++ cloud/backend.py
@@ -1,7 +1,7 @@
 import happybase
 import datetime
 
-HBASE_SERVER = 'ec2-54-227-109-12.compute-1.amazonaws.com'
+HBASE_SERVER = 'ec2-107-21-157-106.compute-1.amazonaws.com'
 TABLE_TWEET = 'tweet'
 TABLE_USER = 'user'
 TABLE_TIME = 'time'
@@ -11,6 +11,12 @@ TABLE_TIME = 'time'
 def time_format(time):
   t = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
   return t.strftime("%a %b %d %H:%M:%S +0000 %Y")
+
+# for q2 encoding and decoding issue
+def utf8_to_unicode(uft8_str):
+  unicode_str = uft8_str.decode('utf8')
+  printable = repr(unicode_str)[1:]
+  return printable[1:-1]
 
 # pass q2 queries to hbase
 def q2_hbase(time):
@@ -28,7 +34,7 @@ def q2_hbase(time):
   table2 = conn.table(TABLE_TWEET)
   result_string = ""
   for key, value in table2.rows(row_keys, columns=["text:text"]):
-    result_string += key + ':' + value["text:text"] +'\n'
+    result_string += key + ':' + utf8_to_unicode(value["text:text"]).replace('/'                                                                             ,'\/') +'\n'
 
   return result_string
 
@@ -36,17 +42,14 @@ def q2_hbase(time):
 def q3_hbase(userid_min, userid_max):
   conn = happybase.Connection(HBASE_SERVER)
   table = conn.table(TABLE_USER)
-  results = []
-  filter_string = "(RowFilter (>=, 'binary:" + str(userid_min) + "') AND RowFilter (<=, 'binary:" + str(userid_max) + "'))"
 
-  for key, value in table.scan(columns=["count:count"],filter=filter_string):
-    if (int(key) >= int(userid_min) and int(key) <= int(userid_max)):
-      result = value['count:count']
-      results.append(result)
+  uid_min = "{:0>20d}".format(long(str(userid_min)))
+  uid_max = "{:0>20d}".format(long(str(userid_max)) + 1L)
 
   sum = 0
-  for i in range(0, len(results)):
-    sum += int(results[i].encode('hex'), 16)
+  for key, value in table.scan(row_start=uid_min, row_stop=uid_max, columns=["co                                                                             unt:count"]):
+    result = value['count:count']
+    sum += int(result.encode('hex'), 16)
 
   return str(sum)+'\n'
 
@@ -54,16 +57,18 @@ def q3_hbase(userid_min, userid_max):
 def q4_hbase(userid):
   conn = happybase.Connection(HBASE_SERVER)
   table = conn.table(TABLE_USER)
-  result = table.row(str(userid), columns=["re:re_uids"])
+
+  uid = "{:0>20d}".format(long(str(userid)))
+  result = table.row(uid, columns=["re:re_uids"])
 
   results = []
-  result_string = ""
   if result:
     results = result["re:re_uids"].split('\n')
 
   results = results[:-1]
   results = sorted(results, key=lambda x: int(x))
 
+  result_string = ""
   for i in range(0, len(results)):
     result_string += results[i]+'\n'
 
